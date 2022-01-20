ef37d850722137212cbd850b2e5ab5af45441af9
xtra-mushroom <maulidavip@gmail.com>
2022-01-20 22:01:47 +0800

    second commit

--- functions.php
+++ functions.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect('localhost', 'lava', 'linolee', 'compact_service');
+$conn = mysqli_connect('localhost', 'username', 'password', 'master_data');
 
 function query($query){
     global $conn;
